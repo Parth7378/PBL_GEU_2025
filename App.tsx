0649c4dc36f118a1fd547f95635c704cdefb4b79
Warriorofliberation11 <nijwalashivam@gmail.com>
2025-05-18 10:36:16 +0530

    Add files via upload

---
+++ App.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import DiskSchedulingSim from "./components/DiskSchedulingSim";
+
+function App() {
+  return (
+    <div className="min-h-screen bg-gray-100 p-4">
+      <DiskSchedulingSim />
+    </div>
+  );
+}
+
+export default App;
